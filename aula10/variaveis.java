8bba079aa5a417985f429dbe6b492cf2b57e1a7e
samayajoyce <joycesamaya@id.uff.br>
2026-02-05 08:17:52 -0300

    aula10:introdução a variaveis

---
+++ aula10/variaveis.java
@@ -0,0 +1,33 @@
+/*
+ * variaveis.java
+ * Copyright 2026 samaya <samaya@samaya-VPCS110GB>
+  aula10 introdução a variaveis
+ */
+
+
+public class variaveis {
+	
+	public static void main (String[] args) {
+		//convençao java
+		int idade = 34;
+		String nome = "Samaya";
+		String nomeDoMeuCachorro;
+		
+		//aceito mas nao usado
+		int _idade;
+		int $idade;
+	
+		// nao é convençao java
+		String nome_d0_meu_cachorro;
+		
+		// má pratica 
+		int a = 10;
+		String b = "samaya"
+		//mostrando na tela
+		System.out.println("idade =" + idade);
+		System.out.println("Nome =" + nome);
+		
+		
+	}
+}
+
